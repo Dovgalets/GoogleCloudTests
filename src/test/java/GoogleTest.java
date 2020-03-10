19a3dcda2a523b984f825c26780df4fe7fa5fafb
SMD <smdov@i.ua>
2020-03-10 10:04:23 +0200

    Without Cucumber

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -26,6 +26,7 @@ public class GoogleTest {
 
         String estimatedPriceFromLetter = GoogleProxy.getPriceFromLetter(PageObjectsFactory.createEmailAddressProviderPage());
 
+
         assertTrue(estimatedPriceFromSite.contains(estimatedPriceFromLetter));
     }
 
